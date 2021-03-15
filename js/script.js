70f2e6c329bbdae69103aeebc7b59daa329a9239
giordanoale <a.giordano89@gmail.com>
2021-03-15 17:36:55 +0100

    Aggiunto slidedown al click

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
-$(".main-nav li").mouseenter(function() {
-  $(this).children(".dropdown-menu").slideDown("fast");
-});
+//Soluzione con Mouse enter e Mouse Leave
+// $(".main-nav li").mouseenter(function() {
+//   $(this).children(".dropdown-menu").slideDown("fast");
+// });
+//
+// $(".main-nav li").mouseleave(function() {
+//   $(this).children(".dropdown-menu").slideUp("fast");
+// });
 
-$(".main-nav li").mouseleave(function() {
-  $(this).children(".dropdown-menu").slideUp("fast");
+
+//Soluzione al Click
+$(".main-nav li").click(function() {
+  $(".dropdown-menu").slideUp("fast");
+  $(this).children(".dropdown-menu").slideToggle("fast");
 });
