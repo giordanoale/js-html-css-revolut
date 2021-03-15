6bea160671842a948a6e3c44904f609c9b363a2f
giordanoale <a.giordano89@gmail.com>
2021-03-15 16:47:39 +0100

    i menu si aprono quando il mouse passa su ogni genitore

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(".main-nav li").mouseenter(function() {
-  $(this).children(".dropdown-menu").slideDown();
+  $(this).children(".dropdown-menu").slideDown("fast");
 });
 
 $(".main-nav li").mouseleave(function() {
-  $(this).children(".dropdown-menu").slideUp();
+  $(this).children(".dropdown-menu").slideUp("fast");
 });
